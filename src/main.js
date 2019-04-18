9ca74e3c0c75cb747fb82d82ef9710ff35910b71
dineug <dineug2@gmail.com>
2019-04-18 21:20:11 +0900

    sentry add

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import './js/fontawesome'
+import './js/sentry'
 
 Vue.config.productionTip = false
 Vue.prototype.$event = new Vue()
